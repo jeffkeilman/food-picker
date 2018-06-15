3ef2aa70b304d268e4b71666a8462a8b5e70f924
Jeff Keilman <jeff.keilman@libertymutual.com>
2018-06-15 11:08:45 -0400

    Added Mongo integration and simple route

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,13 @@
 // routes
 
 module.exports = function(app, db) {
-    app.post('/test', (req, res) => {
-        res.send('test');
+    app.get('/restaurant', (req, res) => {
+        db.collection('restaurants').find({}).toArray((err, collection) => {
+            if (err) res.send(err);
+            else {
+                const randomNum = Math.floor(Math.random() * Math.floor(collection.length));
+                res.json(collection[randomNum]);
+            }
+        });
     });
 };
