c498c3ee9bd00e19652ce7d0721a8f839c0e95f7
Jeff Keilman <jeff.keilman@libertymutual.com>
2018-06-15 11:57:31 -0400

    lol

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,13 +1,43 @@
 // routes
+const https = require('https');
+const url = require('../../config/google-maps').url;
 
 module.exports = function(app, db) {
-    app.get('/restaurant', (req, res) => {
-        db.collection('restaurants').find({}).toArray((err, collection) => {
-            if (err) res.send(err);
-            else {
-                const randomNum = Math.floor(Math.random() * Math.floor(collection.length));
-                res.json(collection[randomNum]);
-            }
-        });
+    // app.get('/restaurant', (req, res) => {
+    //     db.collection('restaurants').find({}).toArray((err, collection) => {
+    //         if (err) res.send(err);
+    //         else {
+    //             const randomNum = Math.floor(Math.random() * Math.floor(collection.length));
+    //             res.json(collection[randomNum]);
+    //         }
+    //     });
+    // });
+
+    app.post('/google-restaurant', (req, res) => {
+        console.log('Hello?');
+        // https.get(url, function(response) {
+        //     let body = '';
+        //     response.on('data', function(chunk) {
+        //         body += chunk;
+        //     });
+
+        //     response.on('end', function() {
+        //         let places = JSON.parse(body);
+        //         let locations = places.results;
+        //         let randLoc = locations[Math.floor(Math.random() * locations.length)];
+        //         res.header("Access-Control-Allow-Origin", "*");
+        //         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        //         const randomRestaurant = {
+        //             name: randLoc.name,
+        //             address: randLoc.formatted_address,
+        //             price: randLoc.price_level
+        //         };
+
+        //         res.json(randomRestaurant);
+        //     });
+        // }).on('error', function(e) {
+        //     console.log("Got error: " + e.message);
+        // });
     });
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-let db = require('./config/db');
+// let db = require('./config/db');
 
 const app = express();
 
@@ -10,13 +10,13 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(db.url, (err, database) => {
-    if (err) return console.log(err);
+// MongoClient.connect(db.url, (err, database) => {
+    // if (err) return console.log(err);
 
-    db = database.db('food-app-thing');
+    // db = database.db('food-app-thing');
     require('./app/routes') (app, db);
 
     app.listen(port, () => {
         console.log('We are live on ' + port);
     });
-});
+// });
